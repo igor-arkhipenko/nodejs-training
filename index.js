18b650c1c2b01714e79fb47e4bf8f9bfff2a9a27
igor-arkhipenko <i.arkhipenko@crystals.ru>
2021-02-20 17:54:08 +0300

    simple web server (http) + html pages

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-type': 'text/plain'
-  })
 
-  res.end('<h1>Hello NodeJS!!!</h1>')
+  // if (req.url === '/') {
+  //   fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+  //     if (err) {
+  //       throw err
+  //     }
+  //
+  //     res.writeHead(200, {
+  //       'Content-type': 'text/html'
+  //     })
+  //     res.end(data)
+  //   })
+  // } else if (req.url === '/contact') {
+  //   fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+  //     if (err) {
+  //       throw err
+  //     }
+  //
+  //     res.writeHead(200, {
+  //       'Content-type': 'text/html'
+  //     })
+  //     res.end(data)
+  //   })
+  // }
+
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+  const ext = path.extname(filePath)
+
+  if (!ext) {
+    filePath += '.html'
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+        if (err) {
+          res.writeHead(500)
+          res.end('Error')
+        } else {
+          res.writeHead(200, {
+            'Content-Type': 'text/html'
+          })
+          res.end(data)
+        }
+      })
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      })
+      res.end(content)
+    }
+  })
 })
 
 server.listen(3000, () => {
